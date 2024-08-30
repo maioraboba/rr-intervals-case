880711f3cd63a7a8d9ad5e6bcfeaf2bbf76d44e4
Roman <113292625+maioraboba@users.noreply.github.com>
2024-08-31 01:34:06 +1000

    Update main_rr.py

--- main_rr.py
+++ main_rr.py
@@ -13,7 +13,7 @@ clf = 'model_rr.pk'
 with open(clf, 'rb') as f:  # открытие обученной модели ML
     loaded_model = pickle.load(f)
 
-data = pd.read_csv("data_rrg_big_xui.csv")
+data = pd.read_csv("data_rrg_big.csv")
 data = data.dropna()
 data_names = data["id"].to_list()
 data = data[list(data.columns[1:-1])].astype(int)
